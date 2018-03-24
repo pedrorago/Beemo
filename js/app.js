0df3c31f23b82d9e72df6b4c51a250acf6b2dc4d
PedroRago10 <30876948+PedroRago10@users.noreply.github.com>
2018-03-24 15:20:42 -0300

    Beemo

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ $(function(){
 
 	setTimeout(saudacao, 2500);
 
+	paraBotao();
+
+	enviaMensagem();
+
 });
 
 
@@ -24,10 +28,17 @@ function saudacao()
 	$(".BeemoBoca").fadeOut(50);  
 	$(".BeemoOlhos").fadeOut(); 
 	$(".BeemoFace").addClass('BeemoFaceDigitando');
+	$(".texto").addClass('textoAlinhado');
 
 	var textoSaudacao = 'Ola amiguinho!';
 
 	setTimeout(digitacao(textoSaudacao), 3000);
+
+	setTimeout(function()
+	{
+		$('.botaoAbreForm').fadeIn();
+		$(".botaoAbreForm").css('animation', 'quicaBotao 1.2s ease-out 3');
+	}, 2000);
 
 }
 
@@ -42,14 +53,46 @@ function digitacao(mensagem)
 		if(n > texto.length) {
 			return false;
 		}
+		setTimeout(function() {
+			alerta();
+		}, v);
+		$('.texto').html(t);
+		n++;
+	}
+
 	setTimeout(function() {
 		alerta();
 	}, v);
-	$('.texto').html(t);
-	n++;
 }
 
-setTimeout(function() {
-	alerta();
-}, v);
+function paraBotao()
+{
+	$('.botaoAbreForm').on('click', function()
+	{	
+		$(this).css('bottom', '3.5%');
+		$(this).css('animation', 'none');
+		abreChat();
+	});
+}
+
+function abreChat()
+{
+	setTimeout(function()
+	{
+		$(".faleForm").toggle();
+
+	}, 200);
+}
+
+function enviaMensagem()
+{
+	var mensagem = $(".faleForm").find('input').val();
+	var btnEnviar = $(".faleForm").find('span');
+	var corpoDoTexto = $(".texto");
+
+	btnEnviar.on('click', function()
+	{
+		var mensagemAEnviar = innerHTML('<p>'+mensagem+'</p>');
+		corpoDoTexto.append(mensagemAEnviar);
+	});
 }
